5d54cb2a07ca400e2a01f0f8dba82da35f30182a
Jake Lyons <Vouch@live.co.uk>
2017-04-06 03:27:18 +0100

    Small Changes plus brain.py
    Main can now handle card positions and remembers the location of
    previous positions to better update current positions. This allows the
    user to flip a card without the program losing track of each
    
    brain.py added to layout the start of the memory class and the core game
    controls

--- camera.py
+++ camera.py
@@ -79,22 +79,25 @@ class Feed:
     def create_rect(self, contour):
         ''' Take a contour and return a rectangle as a list
             so it's compatible with cv2.drawContours '''
-        return [np.int0(cv2.boxPoints(cv2.minAreaRect(contour)))]
-        
-    def draw_contours(self, contours, image):
-        ''' Take contours and draw them on the supplied image '''
-        for con in contours:
-            cv2.drawContours(image, con, -1, (0, 255, 0), 2)
-        return image
+        rect = cv2.minAreaRect(contour)
+        box = cv2.boxPoints(rect)
+        box = [np.int0(box)]
+        return box
     
     def rotate_image(self, image, center, theta, height, width):
-        ''' Change the rotation of an image so that one side is
-            parallel with the screen width (turn upright) '''
+        ''' Change the rotation of an image by theta degrees
+            around the center point '''
         center = (height / 2, width / 2)
         M = cv2.getRotationMatrix2D(center, theta, 1)
         output = cv2.warpAffine(image, M, (height, width))
         return output
-        
+
+    def count_values(self, binary):
+        ''' Takes an image and counts the 0 and >0 values '''
+        white = cv2.countNonZero(binary)
+        black = binary.size - white
+        return white, black
+    
     def compare(self, A, B):
         ''' Take two images and test for similarities '''
         pass
@@ -109,18 +112,20 @@ class Feed:
             screen. Use the following format:
             'Feed.out(name = image)'
             Where name is the window name and image is a numpy array '''
-        if kwargs:
-            for key, value in kwargs.items():
-                cv2.imshow(key, value)
+        for key, value in kwargs.items():
+            cv2.imshow(key, value)
         
-    def end_cycle(self):
+    def end_cycle(self, delay = 1):
         ''' Close all current windows and end the camera feed '''
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            self.cap.release()
-            cv2.destroyAllWindows()
+        if cv2.waitKey(delay) & 0xFF == ord('q'):
+            self.quit()
             return True
         else:
             return False
+
+    def quit(self):
+        self.cap.release()
+        cv2.destroyAllWindows()
 
 
         
@@ -131,24 +136,28 @@ class Geometry:
         ''' Finds the global rotation of the object in relation
             to the side of the camera. 
             '''      
-        center_point = self.find_center(box[0][0], box[0][2])
+        center_point = self.find_center(A = box[0][0],
+                                        B = box[0][2])
         adj_point = (0, center_point[1])
         
-        a = self.pythag(box[0][0], box[0][1])
-        b = self.pythag(box[0][1], box[0][2])
+        a = self.pythag(A = box[0][0], B = box[0][1])
+        b = self.pythag(A = box[0][1], B = box[0][2])
         if a > b:
-            opp_point = self.find_center(box[0][1], box[0][0])
+            opp_point = self.find_center(A = box[0][1],
+                                         B = box[0][0])
         else:
-            opp_point = self.find_center(box[0][1], box[0][2])
+            opp_point = self.find_center(A = box[0][1],
+                                         B = box[0][2])
 
-        a = self.pythag(center_point, opp_point)
-        b = self.pythag(opp_point, adj_point)
-        c = self.pythag(adj_point, center_point)
+        side1 = self.pythag(A = center_point, B = opp_point)
+        side2 = self.pythag(A = opp_point,    B = adj_point)
+        side3 = self.pythag(A = adj_point,    B = center_point)
 
-        #self.draw_traingle(image, (center_point, adj_point, opp_point))
+        #Draw.triangle(image, (center_point, adj_point, opp_point))
 
-        angle = self.SSS(a, b, c)
+        angle = self.SSS(a = side1, b = side2, c = side3)
         angle *= (180 / math.pi)
+        
         if center_point[1] < opp_point[1]:
             angle = 360 - angle
         
@@ -164,29 +173,55 @@ class Geometry:
         ''' Simple equation to find the distance between two points '''
         a = abs(A[0] - B[0])
         b = abs(A[1] - B[1])
-        return int(math.sqrt((a  **  2) + (b  **  2)))
+        root = math.sqrt((a  **  2) + (b  **  2))
+        return int(root)
 
     def SSS(self, a, b, c):
         ''' Simple equation to find the angle using three sides '''
         D = ((c  **  2) + (a  **  2) - (b  **  2)) / (2 * c * a)
-        return math.acos(D)
+        acosine = math.acos(D)
+        return acosine
 
-    def draw_traingle(self, image, points):
+
+class Draw:
+    
+    def contours(self, contours, image):
+        ''' Take contours and draw them on the given image '''
+        for con in contours:
+            cv2.drawContours(image, con, -1, (0, 255, 0), 2)
+    
+    def triangle(self, image, points):
         ''' Draws a triangle from the three points given as well
             as the points themselves for clarity '''
-        lines = [[points[0], points[1]],
-                 [points[1], points[2]],
-                 [points[2], points[0]]]
+
+        lines = [(points[0], points[1]), (points[0], points[2]),
+                 (points[0], points[1]), (points[1], points[2]),
+                 (points[0], points[2]), (points[1], points[2])]
         
         for line in lines:
-            cv2.line(image, line[0], line[1], (255, 255, 0), 2)
+            self.draw_line(image, line[0])
 
         for point in points:
-            cv2.circle(image, point, 2, (0, 0, 255), 2)
+            self.draw_circle(image, point)
+
+    def line(self, image, points):
+        ''' Draws a line on image from the first point to the
+            second point with a thickness of 2 '''
+        cv2.line(image, points[0], points[1], (255, 255, 0), 2)
+        
+    def circle(self, image, center):
+        ''' Draws a circle on image from the center
+            with a radius of 2 '''
+        cv2.circle(image, center, 2, (0, 0, 255), 2)
+
+    def text(self, image, text, position):
+        ''' Draws the specified text onto the image at the
+            specified position '''
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(image, text, position, font, 0.5, (255, 0, 0), 1)
 
 
-
-##      Find Orientation Notes
+##      Find Orientation
 ##    
 ##            This is done using the
 ##            SSS Traingle equation to calculate angles
--- main.py
+++ main.py
@@ -1,57 +1,115 @@
 import camera
 import numpy as np
+
 cam = camera.Feed()
 geo = camera.Geometry()
+draw = camera.Draw()
+
+errors = 0
+cards = {}
+positions = {}
+first_run = True
 
 while(True):
     # Get the BGR frame of the video feed
     colour = cam.store_frame()
     # Convert the BGR image to grayscale
-    gray = cam.to_gray(colour)
+    gray = cam.to_gray(colour = colour)
     # Convert the grayscale image to binary
-    binary = cam.to_binary(gray, 190, 255) #100:255
+    binary = cam.to_binary(gray = gray, lower = 185, upper = 255) #100:255
 
     # Morphologically process the binary image
     #binary = cam.close((5, 5), binary)
 
     # Get the contours from the binary image
-    contours, hierarchy = cam.get_contours(binary)
+    contours, hierarchy = cam.get_contours(binary = binary)
     # Remove the contours which have an area outside the limits
-    contours = cam.remove_contours(contours, 20000, 30000) # 10k:25k
+    contours = cam.remove_contours(contours = contours,
+                                   lower = 20000, upper = 30000) # 10k:25k
     
     # Create box points for each contour, outlining the cards
     boxes = []
     for con in contours:
-        boxes.append(cam.create_rect(con))
-    # Draw the box points to the colour image
-    colour = cam.draw_contours(boxes, colour)
+        boxes.append(cam.create_rect(contour = con))
 
-    # Create separate card images for further processing
-    cards = []
+    if not len(boxes) % 2 == 0 and first_run:
+        print("Uneven number of playing cards detected. Has the board been set up?")
+        cam.quit()
+        break
+   
+    # Draw the box points to the colour image
+    draw.contours(contours = boxes, image = colour)
+
+    i = 0
+    # Create separate card images for further processing 
     for box in boxes:
         # Find the rotation of the box in relation to the screen
-        center, theta = geo.find_orientation(box)
-        y1, x1 = np.amin(box[0], axis=0)
-        y2, x2 = np.amax(box[0], axis=0)
+        center, angle = geo.find_orientation(box = box)
+
+        # Assign each card to a certain index dependant on the
+        # position of the center
+        # This allows better tracking of the cards where before,
+        # the cards had indexes based on how close they were to 
+        # the bottom right which wasn't accurate enough 
+        if first_run:
+            i += 1
+            key = i
+            positions[key] = center
+        else:
+            key = 0
+            nearest = (0, 0)
+            for k, v in positions.items():
+                d1 = geo.pythag(A = center, B = v)
+                d2 = geo.pythag(A = center, B = nearest)
+                
+                if d1 < d2:
+                    nearest = v
+                    key = k
+
+            positions[key] = center
+        
+        y1, x1 = np.amin(box[0], axis = 0)
+        y2, x2 = np.amax(box[0], axis = 0)
         
         h, w = y2 - y1, x2 - x1
         temp = colour[x1:x2, y1:y2]
         try:
-            cards.append(cam.rotate_image(temp, center, theta, h, w))
+            card = cam.rotate_image(image = temp, center = center,
+                                    theta = angle, height = h, width = w)
+            # Potentially crop card to remove background
+            cards[key] = card
         except Exception as e:
             print(e)
-            print("The game board is most likely corrupt.\n")
+            errors += 1
+            print("%s): The game board is most likely corrupt.\n" % errors)
+
+        if i == len(boxes):
+            first_run = False
 
     # Output the colour, grayscale, and binary images
     cam.out_multi(Colour = colour, Grayscale = gray, Binary = binary)
 
     # Output each detected and rotated card
-    i = 0
-    for card in cards:
-        i += 1
-        cam.out_one(str(i), card)
-        
+    for index, card in cards.items():
+        # The following few commands draw a small circle on
+        # cards that are flipped over
+        gr = cam.to_gray(colour = card)
+        bi = cam.to_binary(gray = gr, lower = 190, upper = 255)
 
-    # Safely complete a single loop
+        # Checks if the card is flipped and draws a small
+        # circle in the corner of the window to indicate it
+        w_pixels, b_pixels = cam.count_values(binary = bi)
+        if w_pixels < b_pixels:
+            draw.circle(image = card, center = (20, 20))
+
+        # Draw the center position as text for reference
+        draw.text(image = card, text = str(positions[index]),
+                  position = (10, 60))
+
+        # Remove inner boxes if any (find a better way please)
+        if w_pixels > 0: 
+            cam.out_one(name = str(index), image = card)
+     
+    # Safely complete a single loop and end it if necessary
     if cam.end_cycle():
         break
